7da7ed65895ddf5775f31f5445dad2d3aa02de44
Moopz-Dev <pongzthor@gmail.com>
2022-05-23 11:49:33 +0700

    refactor: seperates firestore module into new file

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -2,18 +2,8 @@ const functions = require("firebase-functions");
 const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
-//firebase imports
-const { getFirestore } = require('firebase-admin/firestore');
-var admin = require("firebase-admin");
-var serviceAccount = require("./service-account.json");
+const fs = require("fs")
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-
-//initialize firebase
-const db = getFirestore();
 
 // initialize app
 const app = express();
@@ -21,8 +11,9 @@ app.use(express.json({ limit: "50mb" }))
 app.use(morgan("tiny"))
 app.use(cors())
 
-//routes
 
+
+//test route
 app.use("/testing", async (req, res, next) => {
   try {
     res.json({ message: "The testing endpoint functions correctly" })
@@ -30,6 +21,9 @@ app.use("/testing", async (req, res, next) => {
     next(error)
   }
 })
+
+//routes
+fs.readdirSync("./routes").map(r => app.use("/api", require("./routes/" + r)))
 
 //error handling
 
@@ -43,8 +37,8 @@ app.use((err, req, res, next) => {
 })
 
 // (Important!)DISABLE THE lINES BELOW BEFORE DEPLOYMENT
-// app.listen(process.env.PORT || 3000, () => {
-//   console.log(`listening on port ${process.env.PORT || 3000}`)
-// })
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`listening on port ${process.env.PORT || 3000}`)
+})
 
 exports.api = functions.https.onRequest(app)
