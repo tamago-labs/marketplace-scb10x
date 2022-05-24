94e5857c410043d97df4beb3700845f7935ec527
pisuthd <pisuth.dae@gmail.com>
2022-05-24 13:18:40 +0900

    refactor: jumbotron

---
+++ frontend/src/components/buttons.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const Button = styled.button.attrs(() => ({ className: "btn btn-secondary shadow" }))`
+    z-index: 10;
+    color: white;
+    border-radius: 32px;
+    padding: 12px 24px;
+`
+
