aa0ff341ff02aabccbf4fff27d1ab58f96a01b94
Moopz-Dev <pongzthor@gmail.com>
2022-05-24 16:01:02 +0700

    feat: adds endpoint to test ether library

--- backend/functions/routes/testRoute.js
+++ backend/functions/routes/testRoute.js
@@ -4,6 +4,7 @@ const testController = require("../controllers/testController")
 
 
 router.get("/test/get", testController.getTestJSON)
+router.get("/test/ethers", testController.testEthers)
 router.post("/test/createMock", testController.createMockData)
 
 module.exports = router;
