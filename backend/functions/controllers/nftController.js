835a8a9913dcda516d6fa519ae5b7c6771dbf47e
Moopz-Dev <pongzthor@gmail.com>
2022-05-27 11:25:35 +0700

    feat : adds getMetadata endpoint

---
+++ backend/functions/controllers/nftController.js
@@ -0,0 +1,46 @@
+const { db } = require("../firebase")
+const { Moralis, MoralisOptions } = require("../moralis")
+const { getMetadata } = require("../utils")
+
+exports.getMetadata = async (req, res, next) => {
+  try {
+
+    if (!req || !req.params) {
+      return res.status(400).json({ message: "missing query params" })
+    }
+    console.log(req.params)
+    const { address, id, chain } = req.params
+
+    if (!address || !id || !chain) {
+      return res.status(400).json({ message: "some or all required fields are missing" })
+    }
+
+    const nft = await db.collection('nfts').where('address', '==', address).where('id', '==', id).where('chain', '==', chain).get()
+
+    if (nft.empty) {
+      // console.log("fetching metadata from Moralis...")
+      await Moralis.start({ ...MoralisOptions })
+
+      const options = {
+        address: address,
+        token_id: id,
+        chain: chain,
+      };
+      const tokenIdMetadata = await Moralis.Web3API.token.getTokenIdMetadata(options);
+      const result = await getMetadata(tokenIdMetadata)
+      const metadata = result.metadata
+      // console.log("saving nft to firestore database...")
+      await db.collection('nfts').add({ address, id, chain, metadata })
+      return res.status(200).json({ status: "ok", metadata })
+    } else {
+      // console.log("fetching metadata from firestore database...")
+      let result = []
+      nft.forEach(doc => {
+        result.push(doc.data())
+      });
+      return res.status(200).json({ status: "ok", metadata: result[0].metadata })
+    }
+  } catch (error) {
+    next(error)
+  }
+}
