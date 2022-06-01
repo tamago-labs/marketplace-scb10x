3332ce44b1b4aac42aefe744b73f15465b84314c
Moopz-Dev <pongzthor@gmail.com>
2022-06-01 15:47:22 +0700

    feat: adds a check for empty meta from API

--- backend/functions/utils/index.js
+++ backend/functions/utils/index.js
@@ -8,12 +8,22 @@ const getMetadata = async (nft) => {
     console.log("no metadata!")
 
     const uri = nft.token_uri.replaceAll("000000000000000000000000000000000000000000000000000000000000000", "")
-    // proxy 
-    const { data } = await axios.get(`https://slijsy3prf.execute-api.ap-southeast-1.amazonaws.com/stage/proxy/${uri}`)
 
-    if (data && data.data) {
-      metadata = data.data
+    if (uri.indexOf("https://") === -1) {
+      uri = `https://${uri}`
     }
+
+    try {
+      // proxy
+      const { data } = await axios.get(`https://slijsy3prf.execute-api.ap-southeast-1.amazonaws.com/stage/proxy/${uri}`)
+
+      if (data && data.data) {
+        metadata = data.data
+      }
+    } catch (e) {
+
+    }
+
   }
 
   return {
