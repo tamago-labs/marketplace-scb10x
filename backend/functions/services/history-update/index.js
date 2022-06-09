979caffdf7714f7b4217b2f06b29be317d21cd05
Moopz-Dev <pongzthor@gmail.com>
2022-06-09 12:25:09 +0700

    feat: tracks user buy-sell activity

--- backend/functions/services/history-update/index.js
+++ backend/functions/services/history-update/index.js
@@ -24,7 +24,7 @@ const updateHistory = async () => {
 
       //COLLECTIONS
       //creating or updating collection doc
-      console.log(order)
+      // console.log(order)
       let collection = await db.collection("collections").where('address', '==', order.baseAssetAddress).where('chainId', '==', order.chainId).get()
 
       if (collection.empty) {
@@ -33,13 +33,17 @@ const updateHistory = async () => {
           address: order.baseAssetAddress,
           chainId: order.chainId,
           activeOrders: [],
-          fulfilledOrders: []
+          activeCount: 0,
+          fulfilledOrders: [],
+          fulfilledCount: 0
         }
         if (order.fulfilled) {
           collectionDoc.fulfilledOrders.push(order.orderId)
+          collectionDoc.fulfilledCount = 1
         }
         else if (order.visible && !order.cancelled) {
           collectionDoc.activeOrders.push(order.orderId)
+          collectionDoc.activeCount = 1
         }
         await db.collection("collections").add(collectionDoc)
       } else {
@@ -48,19 +52,54 @@ const updateHistory = async () => {
           DocID: doc.id,
           ...doc.data(),
         }))[0]
-        console.log(collection)
+        // console.log(collection)
         if (order.fulfilled) {
-          await db.collection("collections").doc(collection.DocID).update({ fulfilledOrders: FieldValue.arrayUnion(order.orderId) })
-          await db.collection("collections").doc(collection.DocID).update({ activeOrders: FieldValue.arrayRemove(order.orderId) })
+          await db.collection("collections").doc(collection.DocID).update({ fulfilledOrders: FieldValue.arrayUnion(order.orderId), fulfilledCount: collection.fulfilledOrders.length })
+          await db.collection("collections").doc(collection.DocID).update({ activeOrders: FieldValue.arrayRemove(order.orderId), activeCount: collection.activeOrders.length })
 
         } else if (order.visible && !order.cancelled) {
-          await db.collection("collections").doc(collection.DocID).update({ activeOrders: FieldValue.arrayUnion(order.orderId) })
+          await db.collection("collections").doc(collection.DocID).update({ activeOrders: FieldValue.arrayUnion(order.orderId), activeCount: collection.activeOrders.length })
         }
       }
 
       //SELLERS
       //creating or updating user doc
-      // let user = await db.collection("users").where('address','==',order.)
+      let user = await db.collection("users").where('address', '==', order.ownerAddress).get()
+
+      if (user.empty) {
+        //creating new firestore collection doc
+        const collectionDoc = {
+          address: order.ownerAddress,
+          activeSells: [],
+          activeSellCount: 0,
+          sold: [],
+          soldCount: 0,
+          // bought: [],
+          // boughtCount: 0
+        }
+        if (order.fulfilled) {
+          collectionDoc.sold.push(order.orderId)
+          collectionDoc.soldCount = 1
+        }
+        else if (order.visible && !order.cancelled) {
+          collectionDoc.activeSells.push(order.orderId)
+          collectionDoc.activeSellCount = 1
+        }
+        await db.collection("users").add(collectionDoc)
+      } else {
+        // updating existing collection doc
+        user = user.docs.map((doc) => ({
+          DocID: doc.id,
+          ...doc.data(),
+        }))[0]
+        if (order.fulfilled) {
+          await db.collection("users").doc(user.DocID).update({ sold: FieldValue.arrayUnion(order.orderId), soldCount: user.sold.length })
+          await db.collection("users").doc(user.DocID).update({ activeSells: FieldValue.arrayRemove(order.orderId), activeSellCount: user.activeSells.length })
+        }
+        else if (order.visible && !order.cancelled) {
+          await db.collection("users").doc(user.DocID).update({ activeSells: FieldValue.arrayUnion(order.orderId), activeSellCount: user.activeSells.length })
+        }
+      }
     }
 
 
