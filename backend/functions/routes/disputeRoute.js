0be1b07813eb2d0aad25c028931d8007e54ec8fa
Moopz-Dev <pongzthor@gmail.com>
2022-06-14 12:51:30 +0700

    chore : disables updateDisputeRoute

--- backend/functions/routes/disputeRoute.js
+++ backend/functions/routes/disputeRoute.js
@@ -4,8 +4,9 @@ const disputeController = require("../controllers/disputeController")
 
 
 router.get("/disputes/", disputeController.getDisputes)
+router.get("/disputes/address/:address", disputeController.getDisputeByAddress)
 router.get("/disputes/:id", disputeController.getDisputeById)
 router.post("/disputes/", disputeController.createDispute)
-router.post("/disputes/update/:id", disputeController.updateDispute)
+// router.post("/disputes/update/:id", disputeController.updateDispute)
 
 module.exports = router;
