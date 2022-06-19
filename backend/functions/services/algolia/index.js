8f7f811faf1c95e01229b0b5086c16a073f6c805
Moopz-Dev <pongzthor@gmail.com>
2022-06-19 16:43:53 +0700

    chore : renames algolia service folder for clarity

--- backend/functions/services/algolia/index.js
+++ backend/functions/services/algolia/index.js
@@ -2,18 +2,18 @@ const algoliasearch = require("algoliasearch")
 require("dotenv").config()
 const { db } = require("../../firebase")
 
-
+const client = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_ADMIN_KEY);
 
 addFirestoreDataToAlgolia = async (req, res) => {
 
   let collections = await db.collection("collections").get()
   collections = collections.docs.map(doc => ({ ...doc.data(), objectID: doc.id }))
 
-  const client = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_ADMIN_KEY);
+
   const index = client.initIndex(process.env.ALGOLIA_INDEX_NAME)
 
   index.saveObjects(collections, (err, data) => {
     res.status(200).send(data)
   })
 }
-module.exports = { addFirestoreDataToAlgolia }
+module.exports = { addFirestoreDataToAlgolia, algoliaClient: client }
