ded3bd2f0a1ad65d35ba799f05ddf8de61031cc4
pisuthd <pisuth.dae@gmail.com>
2022-06-19 20:20:30 +0900

    feat: estimate gas prices

--- services/helper/index.js
+++ services/helper/index.js
@@ -1,3 +1,4 @@
+const { default: axios } = require("axios");
 const { ethers } = require("ethers");
 
 exports.delay = (timer) => {
@@ -27,13 +28,13 @@ exports.getProviders = (chainIds = []) => {
             url = process.env.FUJI_RPC_SERVER
         } else if (chainId === 56) {
             url = process.env.BNB_RPC_SERVER
-        }  else if (chainId === 137) {
+        } else if (chainId === 137) {
             url = process.env.POLYGON_RPC_SERVER
         } else if (chainId === 43114) {
             url = process.env.AVAX_RPC_SERVER
-        } else if ( chainId === 1) {
+        } else if (chainId === 1) {
             url = process.env.MAINNET_RPC_SERVER
-        }   
+        }
 
         if (!url) {
             return
@@ -67,7 +68,7 @@ exports.generateRelayMessages = (items = []) => {
                 if (item.chainId !== chainId) {
                     output.push({
                         orderId,
-                        chainId : item.chainId,
+                        chainId: item.chainId,
                         assetAddress: item.assetAddress,
                         assetTokenIdOrAmount: item.assetTokenIdOrAmount
                     })
@@ -79,3 +80,45 @@ exports.generateRelayMessages = (items = []) => {
     }, [])
 }
 
+
+exports.getGasPrices = async (chainId) => {
+    let BASE_GAS = 5 // 5 GWEI
+    let gasLimit = 100000
+
+    if ([43113].includes(chainId)) {
+        BASE_GAS = 27
+        gasLimit = 200000
+    }
+
+    try {
+
+        let url
+
+        switch (chainId) {
+            case 1:
+                url = "https://owlracle.info/eth/gas?apikey=cf72e23d385a45639f67e89094cf8eab"
+                break
+            case 137:
+                url = "https://owlracle.info/poly/gas?apikey=cf72e23d385a45639f67e89094cf8eab"
+                break
+            case 43114:
+                url = "https://owlracle.info/avax/gas?apikey=cf72e23d385a45639f67e89094cf8eab"
+                break
+        }
+
+        if (url) {
+            const { data } = await axios.get(url)
+            
+            BASE_GAS = Math.ceil(data.speeds[1].gasPrice)
+            gasLimit = BASE_GAS *10000
+        }
+
+    } catch (e) {
+        console.log(e)
+    }
+
+    return {
+        BASE_GAS,
+        gasLimit
+    }
+}
