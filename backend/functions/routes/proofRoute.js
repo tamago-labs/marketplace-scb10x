0f45d68e8070349ba136839272c95dd12795d51f
Moopz-Dev <pongzthor@gmail.com>
2022-06-23 18:40:28 +0700

    fix: corrects URL path to lowercase

--- backend/functions/routes/proofRoute.js
+++ backend/functions/routes/proofRoute.js
@@ -7,7 +7,7 @@ router.post("/proof/swap/", proofController.generateSwapProof)
 router.post("/proof/partial-swap", proofController.generatePartialSwapProof)
 router.post("/proof/claim", proofController.generateClaimProof)
 router.get("/proof/relay-messages", proofController.generateRelayMessages)
-router.get("/proof/Validator-messages", proofController.generateValidatorMessages)
+router.get("/proof/validator-messages", proofController.generateValidatorMessages)
 
 
 module.exports = router;
