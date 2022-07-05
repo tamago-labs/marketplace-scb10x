e7a25d130ecb96c6dc51d712753dafc0746c3555
Jomjam <jomjam.kusumaj@gmail.com>
2022-07-05 22:51:28 +0700

    ✨ add CoinGecko api and add floor price

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -5,6 +5,8 @@ export const API_BASE =
 export const ACCOUNT_API_BASE =
   "https://asia-east2-sbc10x-hackatron-may2022.cloudfunctions.net/api";
 
+export const COIN_GECKO_API_BASE = "https://api.coingecko.com/api/v3";
+
 export const SUPPORT_CHAINS = [42, 97, 80001, 43113, 137, 56, 43114, 1];
 
 export const MAINNET_CHAINS = [137, 56, 43114, 1];
