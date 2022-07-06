a9cacb55f9b76a6df20e10d4df59aac14fd0e932
Moopz-Dev <pongzthor@gmail.com>
2022-07-06 10:51:51 +0700

    chore: disables orderCancel endpoint

--- backend/functions/routes/orderRoute.js
+++ backend/functions/routes/orderRoute.js
@@ -6,12 +6,10 @@ const orderController = require("../controllers/orderController")
 router.get("/orders", orderController.getOrders)
 router.post("/orders", orderController.createOrder)
 router.post("/orders/confirm", orderController.confirmOrder)
-router.post("/orders/cancel", orderController.cancelOrder)
+// router.post("/orders/cancel", orderController.cancelOrder)
 router.get("/orders/collection/:address", orderController.getOrdersByCollection)
 router.get("/orders/owner/:owner", orderController.getOrdersByOwner)
 router.get("/orders/:id", orderController.getOrder)
-
-
 
 
 module.exports = router;
