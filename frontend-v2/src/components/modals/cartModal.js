1f6204e84fe0c5d18619c9bee4d8d4e1e4f74d2f
Jomjam <jomjam.kusumaj@gmail.com>
2022-07-30 21:59:07 +0700

    💄  add cart

--- frontend-v2/src/components/modals/cartModal.js
+++ frontend-v2/src/components/modals/cartModal.js
@@ -1,15 +1,31 @@
 import React, { useState } from "react";
 // import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import { ShoppingCart } from "react-feather";
+import { ShoppingCart, ArrowRight } from "react-feather";
 import styled from "styled-components";
 import { Button } from "../button";
+
+const Preview = styled.div`
+  display: flex;
+  color: #333;
+  div {
+    flex: 1;
+    width: 150px;
+    border-radius: 12px;
+    padding: 12px;
+    border: 1px solid transparent;
+    margin-left: 3px;
+    margin-right: 3px;
+    display: flex;
+  }
+`;
 
 const CartModal = () => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
@@ -21,9 +37,35 @@ const CartModal = () => {
           <Modal.Title>Your NFT Shopping Cart</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <ul>1</ul>
-          <ul>1</ul>
-          <ul>1</ul>
+          <ul>
+            <Preview>
+              #1
+              <div>
+                <img
+                  src={"../images/coin.png"}
+                  width="80px"
+                  height="80px"
+                  style={{ margin: "auto" }}
+                />
+              </div>
+              <div>
+                <div style={{ margin: "auto", textAlign: "center" }}>
+                  <ArrowRight
+                    style={{ marginLeft: "auto", marginRight: "auto" }}
+                    size={32}
+                  />
+                </div>
+              </div>
+              <div>
+                <img
+                  src={"../images/coin.png"}
+                  width="80px"
+                  height="80px"
+                  style={{ margin: "auto" }}
+                />
+              </div>
+            </Preview>
+          </ul>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
