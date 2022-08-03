23f9b38afb4d45833fdb902fe224c753f499a889
Moopz-Dev <pongzthor@gmail.com>
2022-08-03 17:28:50 +0700

    fix : corrects wrong variable usage

--- backend/functions/v2/controllers/collections.js
+++ backend/functions/v2/controllers/collections.js
@@ -39,7 +39,7 @@ exports.getCollectionsByChain = async (req, res, next) => {
     }
     const collections = await collectionModel.getCollectionsByChain(chain)
     for (const collection of collections) {
-      collection.lowestPrice = await collectionModel.getFloorPrice(collection.chainId, collection.baseAssetAddress)
+      collection.lowestPrice = await collectionModel.getFloorPrice(collection.chainId, collection.assetAddress)
     }
     return res.json({ status: "ok", collections })
   } catch (error) {
