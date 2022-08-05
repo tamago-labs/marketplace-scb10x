e437da3043e675f2ca4c0bc3213e561c32cc0262
Moopz-Dev <pongzthor@gmail.com>
2022-08-05 09:09:34 +0700

    refactor : moves RPC server urls to constant file and refactor into array

--- backend/functions/constants.js
+++ backend/functions/constants.js
@@ -54,6 +54,19 @@ const CLIENT_BASE = "https://marketplace-10x.tamago.finance"
 
 const COIN_GECKO_API_BASE = "https://api.coingecko.com/api/v3";
 
+POLYGON_RPC_SERVERS = ["https://nd-643-057-168.p2pify.com/2ffe10d04df48d14f0e9ff6e0409f649"]
+MAINNET_RPC_SERVERS = ["https://nd-814-913-142.p2pify.com/cb3fe487ef9afa11bda3c38e54b868a3"]
+KOVAN_RPC_SERVERS = ["https://kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"]
+MUMBAI_RPC_SERVERS = ["https://nd-546-345-588.p2pify.com/8947d77065859cda88213b612a0f8679"]
+BNB_RPC_SERVERS = ["https://nd-886-059-484.p2pify.com/b62941033adcd0358ff9f38df217f856"]
+FUJI_RPC_SERVERS = ["https://nd-473-270-876.p2pify.com/613a7805f3d64a52349b6ca19b6e27a7/ext/bc/C/rpc"]
+BNB_TESTNET_RPC_SERVERS = ["https://nd-390-191-961.p2pify.com/0645132aa2a233d3fbe27116f3b8828b"]
+AVALANCHE_C_CHAIN_RPC_SERVERS = ["https://nd-752-163-197.p2pify.com/fd84ccbd64f32d8f8a99adb5d4557b0e/ext/bc/C/rpc"]
+//TODO add cronos here
+
+
+
+
 module.exports = {
     MARKETPLACES,
     SUPPORTED_CHAINS,
@@ -61,5 +74,14 @@ module.exports = {
     TESTNET_CHAINS,
     WHITELISTED_ADDRESSES,
     CLIENT_BASE,
-    COIN_GECKO_API_BASE
+    COIN_GECKO_API_BASE,
+    //RPC SERVERS
+    POLYGON_RPC_SERVERS,
+    MAINNET_RPC_SERVERS,
+    KOVAN_RPC_SERVERS,
+    MUMBAI_RPC_SERVERS,
+    BNB_RPC_SERVERS,
+    FUJI_RPC_SERVERS,
+    BNB_TESTNET_RPC_SERVERS,
+    AVALANCHE_C_CHAIN_RPC_SERVERS
 }
