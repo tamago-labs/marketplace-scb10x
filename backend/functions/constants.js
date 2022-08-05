7a834e7f1b5936f002169fc287d8ebf8f19928df
Moopz-Dev <pongzthor@gmail.com>
2022-08-05 09:50:16 +0700

    feat : adds new set of rpc server urls

--- backend/functions/constants.js
+++ backend/functions/constants.js
@@ -54,15 +54,55 @@ const CLIENT_BASE = "https://marketplace-10x.tamago.finance"
 
 const COIN_GECKO_API_BASE = "https://api.coingecko.com/api/v3";
 
-POLYGON_RPC_SERVERS = ["https://nd-643-057-168.p2pify.com/2ffe10d04df48d14f0e9ff6e0409f649"]
-MAINNET_RPC_SERVERS = ["https://nd-814-913-142.p2pify.com/cb3fe487ef9afa11bda3c38e54b868a3"]
-KOVAN_RPC_SERVERS = ["https://kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"]
-MUMBAI_RPC_SERVERS = ["https://nd-546-345-588.p2pify.com/8947d77065859cda88213b612a0f8679"]
-BNB_RPC_SERVERS = ["https://nd-886-059-484.p2pify.com/b62941033adcd0358ff9f38df217f856"]
-FUJI_RPC_SERVERS = ["https://nd-473-270-876.p2pify.com/613a7805f3d64a52349b6ca19b6e27a7/ext/bc/C/rpc"]
-BNB_TESTNET_RPC_SERVERS = ["https://nd-390-191-961.p2pify.com/0645132aa2a233d3fbe27116f3b8828b"]
-AVALANCHE_C_CHAIN_RPC_SERVERS = ["https://nd-752-163-197.p2pify.com/fd84ccbd64f32d8f8a99adb5d4557b0e/ext/bc/C/rpc"]
-//TODO add cronos here
+// RPC server urls retrieved from chainlist.org
+POLYGON_RPC_SERVERS = [
+    "https://polygon-mainnet.public.blastapi.io",
+    "https://polygon-rpc.com",
+    "https://polygonapi.terminet.io/rpc",
+    "https://rpc-mainnet.matic.quiknode.pro",
+]
+
+MAINNET_RPC_SERVERS = [
+    "https://eth-rpc.gateway.pokt.network",
+    "https://eth-mainnet.public.blastapi.io",
+    "https://rpc.ankr.com/eth",
+]
+
+//note: no good rpc servers on chainlist (using the old infura server for now)
+KOVAN_RPC_SERVERS = [
+    "https://kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+]
+
+MUMBAI_RPC_SERVERS = [
+    "https://polygontestapi.terminet.io/rpc",
+    "https://rpc.ankr.com/polygon_mumbai",
+]
+
+BNB_RPC_SERVERS = [
+    "https://bsc-dataseed2.binance.org",
+    "https://bscapi.terminet.io/rpc",
+    "https://rpc.ankr.com/bsc",
+    "https://bsc-dataseed3.binance.org",
+]
+
+FUJI_RPC_SERVERS = [
+    "https://rpc.ankr.com/avalanche_fuji",
+    "https://avalanchetestapi.terminet.io/ext/bc/C/rpc",
+]
+
+BNB_TESTNET_RPC_SERVERS = [
+    "https://bsctestapi.terminet.io/rpc",
+    "https://data-seed-prebsc-2-s2.binance.org:8545",
+    "https://data-seed-prebsc-1-s1.binance.org:8545",
+]
+AVALANCHE_C_CHAIN_RPC_SERVERS = [
+    "https://rpc.ankr.com/avalanche",
+    "https://avalancheapi.terminet.io/ext/bc/C/rpc",
+    "https://ava-mainnet.public.blastapi.io/ext/bc/C/rpc",
+]
+CRONOS_RPC_SERVERS = ["https://cronosrpc-1.xstaking.sg",
+    "https://evm.cronos.org",
+]
 
 
 
@@ -83,5 +123,6 @@ module.exports = {
     BNB_RPC_SERVERS,
     FUJI_RPC_SERVERS,
     BNB_TESTNET_RPC_SERVERS,
-    AVALANCHE_C_CHAIN_RPC_SERVERS
+    AVALANCHE_C_CHAIN_RPC_SERVERS,
+    CRONOS_RPC_SERVERS
 }
